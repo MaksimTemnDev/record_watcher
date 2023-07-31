3ea2da4595d5726db36df53def8f8fcbed2d1623
Темненков Максим <temnenkovmaks@gmail.com>
2023-07-31 17:14:29 +0300

    useless class MainViewModel deleted

--- app/src/main/java/com/example/record_watcher/api/MainViewModel.kt
+++
@@ -1,8 +0,0 @@
-package com.example.record_watcher.api
-
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-
-class MainViewModel: ViewModel() {
-    val liveDataCurrent = MutableLiveData<MailModel>()
-}
